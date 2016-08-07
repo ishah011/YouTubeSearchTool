8483944a762c53f0007b3b375501442aa39dce51
Isheeta Shah <isheeta_s@yahoo.com>
2016-08-07 00:02:29 -0500

    added sorting by rating, views, and year uploaded
    takes input from user for search query and sorting method

--- youtubeapitest.py
+++ youtubeapitest.py
@@ -16,15 +16,29 @@ YOUTUBE_API_VERSION = "v3"
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
+query = raw_input('What would you like to search for?')
+sorting = raw_input('What would you like to sort the results by, rating, views, or year uploaded?')
+
+if(sorting == 'rating'):
+	sort = "rating"
+elif(sorting == 'views'):
+	sort = "viewCount"
+elif(sorting == 'year uploaded'):
+	sort = "date"
+else:
+	print "The input is not rating, views, or year uploaded. Input is case-sensitive. Using default value of rating."
+	sort = "rating"
+
 # Call the search.list method to retrieve results matching the specified
 # query term.
-search_response = youtube.search().list(
-  q="puppy",
+search_rating_response = youtube.search().list(
+  q= query,
   part="snippet",
-  maxResults=2,
-  type="video"
+  maxResults=5,
+  type="video",
+  order= sort
 ).execute()
 
 # search_response["items"][0]["snippet"]["title"]
-for video in search_response["items"]:
-  print video["snippet"]["title"] + " - " + video["snippet"]["channelTitle"]
+for video in search_rating_response["items"]:
+	print video["snippet"]["title"] + " :: " + video["snippet"]["channelTitle"]
